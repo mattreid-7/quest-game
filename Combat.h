4aa1805392ed1c78e0d5ada12e87570b3210158e
mattreid-7 <36977621+mattreid-7@users.noreply.github.com>
2018-06-28 13:39:55 +1000

    Create Combat.h

---
+++ Combat.h
@@ -0,0 +1,37 @@
+#ifndef COMBAT_H
+#define COMBAT_H
+
+#include "Game.h"
+
+
+class Combat
+{
+public:
+	Combat();
+	virtual ~Combat();
+
+	//Operators
+
+	//Accessors
+	
+	//Functions
+	void Abilities();
+	void Armours();
+	void Consumables();
+	void Creatures();
+	void CreatureAttack();
+	void DeathCheck();
+	void EnemyDeathCheck();
+	void ExtraAttack();
+	void Fighting();
+	void Initiative();
+	void LvlUpCheck();
+	void Spells();
+	void Weapons();
+	
+	//Variables
+
+
+};
+#endif
+
