f6d23db49cd2aff6bfe2451628ee5fe809b880c7
mattreid-7 <36977621+mattreid-7@users.noreply.github.com>
2018-06-28 13:38:57 +1000

    Create Combat.cpp

---
+++ Combat.cpp
@@ -0,0 +1,68 @@
+//Includes all of the abilities and anything tied to combat.
+
+#include "stdafx.h"
+#include "Combat.h"
+
+
+Combat::Combat()
+{
+}
+
+Combat::~Combat()
+{
+
+}
+
+
+//Functions
+void Combat::Abilities() {
+
+}
+
+void Combat::Armours() {
+
+}
+
+void Combat::Consumables() {
+
+}
+
+void Combat::Creatures() {
+
+}
+
+void Combat::CreatureAttack() {
+
+}
+
+void Combat::DeathCheck() {
+
+}
+
+void Combat::EnemyDeathCheck() {
+
+}
+
+void Combat::ExtraAttack() {
+
+}
+
+void Combat::Fighting() {
+
+}
+
+void Combat::Initiative() {
+
+}
+
+void Combat::LvlUpCheck() {
+
+}
+
+void Combat::Spells() {
+
+}
+
+void Combat::Weapons() {
+
+}
